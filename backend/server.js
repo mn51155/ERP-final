843cf1a9ade82b17b8cfe9dcb11c17b06ef2569d
masoumeNazari <mn51155@gmail.com>
2025-11-23 17:20:44 +0100

    Fix backend:remove frontend imports and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 const cors = require('cors')
-const { applyMiddleware } = require('@reduxjs/toolkit')
+
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
